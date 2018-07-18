895c923bef3fb21abc81af97712930aaf0fc7c08
Pauline Trung <pauline.trung@aon.at>
2018-07-18 09:38:22 +0200

    Add error handling

--- app.js
+++ app.js
@@ -18,7 +18,13 @@ request({
   url: `https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyDJsLBq_gRyVWbnI2NMoBd_iGm57FX6_KA&address=${encodedAddress}`,
   json: true
 }, (error, response, body) => {
-  console.log(body.results[0].formatted_address);
-  console.log(body.results[0].geometry.location.lat);
-  console.log(body.results[0].geometry.location.lng);
+  if (error) {
+    console.log('Unable to connect to Google servers.');
+  } else if (body.status === 'ZERO_RESULTS') {
+    console.log('Unable to find that address.');
+  } else if (body.status === 'OK') {
+    console.log(body.results[0].formatted_address);
+    console.log(body.results[0].geometry.location.lat);
+    console.log(body.results[0].geometry.location.lng);
+  }
 });
