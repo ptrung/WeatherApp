99cfd12ca177ebf05dcda94e07fb32eb2f1f0b7c
Pauline Trung <pauline.trung@aon.at>
2018-07-18 09:15:47 +0200

    Print adress, lat and lng

--- app.js
+++ app.js
@@ -4,5 +4,7 @@ request({
   url: 'https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyDJsLBq_gRyVWbnI2NMoBd_iGm57FX6_KA&address=Salzburg',
   json: true
 }, (error, response, body) => {
-  console.log(body);
-})
+  console.log(body.results[0].formatted_address);
+  console.log(body.results[0].geometry.location.lat);
+  console.log(body.results[0].geometry.location.lng);
+});
