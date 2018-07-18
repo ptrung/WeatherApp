14ef8a8bf1d50471661350c9a3de239669ebab42
Pauline Trung <pauline.trung@aon.at>
2018-07-18 09:51:59 +0200

    Create Callbacks

--- geocode/geocode.js
+++ geocode/geocode.js
@@ -1,6 +1,6 @@
 const request = require('request');
 
-module.exports.geocodeAddress = (address) => {
+module.exports.geocodeAddress = (address, callback) => {
   var encodedAddress = encodeURIComponent(address);
 
   request({
@@ -8,13 +8,15 @@ module.exports.geocodeAddress = (address) => {
     json: true
   }, (error, response, body) => {
     if (error) {
-      console.log('Unable to connect to Google servers.');
+      callback('Unable to connect to Google servers.');
     } else if (body.status === 'ZERO_RESULTS') {
-      console.log('Unable to find that address.');
+      callback('Unable to find that address.');
     } else if (body.status === 'OK') {
-      console.log(body.results[0].formatted_address);
-      console.log(body.results[0].geometry.location.lat);
-      console.log(body.results[0].geometry.location.lng);
+      callback(undefined, {
+        address: body.results[0].formatted_address,
+        lat: body.results[0].geometry.location.lat,
+        lng: body.results[0].geometry.location.lng
+      });
     }
   });
 }
